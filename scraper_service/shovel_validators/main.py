8165b44de6d29898e9786e3351e4d32a6f388f1c
hey Michal! <michal@opentensor.dev>
2025-03-13 00:51:05 +0100

    properly parsing data

--- scraper_service/shovel_validators/main.py
+++ scraper_service/shovel_validators/main.py
@@ -10,16 +10,25 @@ from shared.exceptions import DatabaseConnectionError, ShovelProcessingError
 import logging
 from typing import Dict, List, Any, Optional
 from functools import lru_cache
+from typing import Union
+from scalecodec.utils.ss58 import ss58_encode
+
+SS58_FORMAT = 42
+
+def decode_account_id(account_id_bytes: Union[tuple[int], tuple[tuple[int]]]):
+    if isinstance(account_id_bytes, tuple) and isinstance(account_id_bytes[0], tuple):
+        account_id_bytes = account_id_bytes[0]
+    return ss58_encode(bytes(account_id_bytes).hex(), SS58_FORMAT)
 
 logging.basicConfig(level=logging.INFO,
                    format="%(asctime)s %(process)d %(message)s")
 
 COMPOUNDING_PERIODS_PER_DAY = 7200
 
-def create_validators_table():
-    if not table_exists("validators"):
-        query = """
-        CREATE TABLE IF NOT EXISTS validators (
+def create_validators_table(table_name):
+    if not table_exists(table_name):
+        query = f"""
+        CREATE TABLE IF NOT EXISTS {table_name} (
             block_number UInt64,
             timestamp DateTime,
             name String,
@@ -37,6 +46,7 @@ def create_validators_table():
         ) ENGINE = ReplacingMergeTree()
         ORDER BY (block_number, address)
         """
+
         get_clickhouse_client().execute(query)
 
 def calculate_apy_from_daily_return(return_per_1000: float, compounding_periods: int = COMPOUNDING_PERIODS_PER_DAY) -> float:
@@ -50,12 +60,12 @@ def create_storage_key_cached(pallet, storage, args):
 
 def get_subnet_uids(substrate, block_hash: str) -> List[int]:
     try:
-        key = create_storage_key_cached("SubnetInfoRuntimeApi", "get_subnets_info", [])
-        result = substrate.rpc_request(
-            "state_getStorage",
-            params=[key, block_hash]
+        result = substrate.runtime_call(
+            api="SubnetInfoRuntimeApi",
+            method="get_subnets_info",
+            params=[]
         )
-        subnet_info = result.get("result", [])
+        subnet_info = result
         return [info['netuid'] for info in subnet_info if 'netuid' in info]
     except Exception as e:
         logging.error(f"Failed to get subnet UIDs: {str(e)}")
@@ -63,13 +73,14 @@ def get_subnet_uids(substrate, block_hash: str) -> List[int]:
 
 def get_active_validators(substrate, block_hash: str) -> List[str]:
     try:
-        key = create_storage_key_cached("DelegateInfoRuntimeApi", "get_delegates", [])
-        result = substrate.rpc_request(
-            "state_getStorage",
-            params=[key, block_hash]
+        result = substrate.runtime_call(
+            api="DelegateInfoRuntimeApi",
+            method="get_delegates",
+            params=[]
         )
-        delegate_info = result.get("result", [])
-        return [delegate['delegateSs58'] for delegate in delegate_info if 'delegateSs58' in delegate]
+
+        delegate_info = result.value
+        return [decode_account_id(delegate['delegate_ss58']) for delegate in delegate_info if 'delegate_ss58' in delegate]
     except Exception as e:
         logging.error(f"Failed to get active validators: {str(e)}")
         return []
@@ -94,13 +105,13 @@ def get_total_hotkey_alpha(substrate, address: str, net_uid: int) -> float:
 
 def fetch_validator_info(substrate, address: str, block_hash: str) -> Dict[str, Any]:
     try:
-        key = create_storage_key_cached("DelegateInfoRuntimeApi", "get_delegates", [])
-        result = substrate.rpc_request(
-            "state_getStorage",
-            params=[key, block_hash]
+        result = substrate.runtime_call(
+            api="DelegateInfoRuntimeApi",
+            method="get_delegates",
+            params=[]
         )
-        delegate_info = result.get("result", [])
-        chain_info = next((d for d in delegate_info if d['delegateSs58'] == address), None)
+        delegate_info = result
+        chain_info = next((d for d in delegate_info if d['delegate_ss58'] == address), None)
 
         if not chain_info:
             return {
@@ -138,13 +149,13 @@ def fetch_validator_info(substrate, address: str, block_hash: str) -> Dict[str, 
 
 def fetch_validator_stats(substrate, address: str, block_hash: str) -> Dict[str, Any]:
     try:
-        key = create_storage_key_cached("DelegateInfoRuntimeApi", "get_delegates", [])
-        result = substrate.rpc_request(
-            "state_getStorage",
-            params=[key, block_hash]
+        result = substrate.runtime_call(
+            api="DelegateInfoRuntimeApi",
+            method="get_delegates",
+            params=[]
         )
-        delegate_info = result.get("result", [])
-        info = next((d for d in delegate_info if d['delegateSs58'] == address), None)
+        delegate_info = result
+        info = next((d for d in delegate_info if d['delegate_ss58'] == address), None)
 
         if not info:
             return {
@@ -188,6 +199,8 @@ def fetch_validator_stats(substrate, address: str, block_hash: str) -> Dict[str,
         }
 
 class ValidatorsShovel(ShovelBaseClass):
+    table_name = "shovel_validators"
+
     def __init__(self, name):
         super().__init__(name)
         self.starting_block = 4920351
@@ -197,7 +210,7 @@ class ValidatorsShovel(ShovelBaseClass):
             substrate = get_substrate_client()
             (block_timestamp, block_hash) = get_block_metadata(n)
 
-            create_validators_table()
+            create_validators_table(self.table_name)
             validators = get_active_validators(substrate, block_hash)
 
             for validator_address in validators:
@@ -222,7 +235,9 @@ class ValidatorsShovel(ShovelBaseClass):
                         stats["subnet_hotkey_alpha"]
                     ]
 
-                    buffer_insert("validators", values)
+                    print(values)
+
+                    buffer_insert(self.table_name, values)
 
                 except Exception as e:
                     logging.error(f"Error processing validator {validator_address}: {str(e)}")
